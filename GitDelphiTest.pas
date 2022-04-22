7610838ecd166ed48d7b25d8c10a5c334f78e5d0
q01057980716@gmail.com <greenque-ko>
2022-04-22 13:59:18 +0900

    Visual binding

--- GitDelphiTest.pas
+++ GitDelphiTest.pas
@@ -5,12 +5,21 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
-  FMX.Controls.Presentation, FMX.StdCtrls;
+  FMX.Controls.Presentation, FMX.StdCtrls, FMX.ListView.Types,
+  FMX.ListView.Appearances, FMX.ListView.Adapters.Base, System.Rtti,
+  System.Bindings.Outputs, Fmx.Bind.Editors, Data.Bind.EngExt,
+  Fmx.Bind.DBEngExt, Data.Bind.Components, Data.Bind.DBScope, Data.DB,
+  Datasnap.DBClient, FMX.ListView;
 
 type
   TGitTestPrj = class(TForm)
     SpeedButton1: TSpeedButton;
     StyleBook1: TStyleBook;
+    ListView1: TListView;
+    ClientDataSet1: TClientDataSet;
+    BindSourceDB1: TBindSourceDB;
+    BindingsList1: TBindingsList;
+    LinkListControlToField1: TLinkListControlToField;
   private
     { Private declarations }
   public
